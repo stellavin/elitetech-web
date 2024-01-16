519d69baeacf14ca850257e21430641ea4e9349c
Stella Sikhila <stellavinmaris@gmail.com>
2024-01-16 19:42:09 +0300

    added port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 8080
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
 
