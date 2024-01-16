0366695cb211b4779d6cea5ea357082fa1580560
Stella Sikhila <stellavinmaris@gmail.com>
2024-01-16 19:57:06 +0300

    fixed server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const next = require('next');
 
 
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
-const port = 8080
+const hostname = '0.0.0.0'
+const port = process.env.PORT || 8080;
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
 
